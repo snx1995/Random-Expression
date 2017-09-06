ffa902bf2df5bfcc982bbb4c8a76953b6cbefd5c
snx1995 <snx1788@gmail.com>
2017-09-06 17:05:47 +0800

    Class Requirement first defined

--- src/randExpression/ExpresTest.java
+++ src/randExpression/ExpresTest.java
@@ -4,19 +4,31 @@ public class ExpresTest {
 
 }
 
+class Requirement {
+	public int range;
+	public boolean negative;
+	public boolean multiOrDiv;
+
+	public Requirement(int Ran, boolean N, boolean MOD) {
+		range = Ran;
+		negative = N;
+		multiOrDiv = MOD;
+	}
+}
+
 class Expression {
 	private String expression;
-	private String result;
+	private int result;
 
-	public Expression(String E,String R) {
-		expression=E;
+	public Expression(String E, String R) {
+		expression = E;
 		result = R;
 	}
-	
+
 	public String getExpression() {
 		return expression;
 	}
-	
+
 	public String toString() {
 		return expression + " = " + result;
 	}
